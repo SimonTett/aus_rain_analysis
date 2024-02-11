89ea5a2a3cd4b7ceef4ca0cf92b0cb29f482c5ac
Simon Tett <simon.tett@ed.ac.uk>
2024-02-11 19:11:26 +1100

    Added code to deal with GSDP meta data and data

--- ausLib.py
+++ ausLib.py
@@ -5,12 +5,205 @@ import numpy as np
 import xarray
 import typing
 import logging
-import resource
+import pandas as pd
+
 
 radar_dir = pathlib.Path("/g/data/rq0/level_2/")
 
 my_logger = logging.getLogger(__name__)  # for logging
 
+def gsdp_metadata(files:typing.Iterable[pathlib.Path])  -> pd.DataFrame:
+    """
+    Read metadata from multiple files into a DataFame. See read_gdsp_metadata for description of columns.
+    Args:
+        files (): List of files to read in
+
+    Returns: DataFrame of metadata.
+    """
+    series=[]
+    for file in files:
+        try:
+            s = read_gsdp_metadata(file)
+            series.append(s)
+        except ValueError: # some problem with data.
+            pass
+    df = pd.DataFrame(series)
+    return df
+def read_gsdp_metadata(file:pathlib.Path) -> pd.Series:
+    """
+      read metadata from hourly precip file created for  gsdp (Global sub-daily precip).
+      Format of header data is:
+      Station ID:  AU_090180, -> ID
+     Country:  Australia,
+     Original Station Number:  090180,
+     Original Station Name:  AIREYS INLET,
+     Path to original data:  B:/INTENSE data/Original data/Australia/AWS 1min_VIC.zip/HD01D_Data_090180_999999998747801.txt,
+     Latitude:  -38.4583,
+     Longitude:  144.0883,
+     Start datetime:  2007072014, -> Start
+     End datetime:  2015082009, -> End
+     Elevation:  95.0m, -> height & height units
+     Number of records:  70868,
+     Percent missing data:  7.45,
+     Original Timestep:  1min,
+     New Timestep:  1hr, -> Timestep
+     Original Units:  mm,
+     New Units:  mm, -> units
+     Time Zone:  CET, -> timezone
+     Daylight Saving info:  NA,
+     No data value:  -999
+     Resolution:  0.20,
+     Other:
+
+    """
+    result=dict()
+    with open(file, 'r') as fh:
+        hdr_count=0
+        while True:
+            key,value = fh.readline().strip().split(":",maxsplit=1)
+            # on first line expect key to be 'Station ID'. if not raise an error.
+            if (hdr_count == 0) & (key != 'Station ID'):
+                raise ValueError(f"File {file} has strange first line {key}:{value}")
+            value = value.strip()
+            result[key]=value
+            hdr_count += 1
+            if key == 'Other': # end fo header
+                break # all done
+
+    # convert same values to loads
+    for key in ['Latitude','Longitude','Resolution','Percent missing data','No data value']:
+        result[key]=float(result[key])
+    # integer vales
+    for key in ['Number of records']:
+        result[key]=int(result[key])
+    # convert ht.
+    result['height']=float(result['Elevation'][0:-1])
+    result['height units']=result['Elevation'][-1]
+    # rename stuff
+    # add in str version of path
+    result['file'] = str(file)
+    # Store number of header lines -- makes reading in data easier.
+    result['header_lines'] = hdr_count
+    rename={'New Units':'units',
+            'Station ID':'ID',
+            'Start datetime':'Start',
+            'End datetime':'End',
+            'Time Zone':'timezone',
+            'New Timestep':'timestep'}
+    for old_name, new_name in rename.items():
+        result[new_name]=result.pop(old_name)
+    # make Start & End datetime
+    start = result.pop('Start')
+    start = start[0:4]+"-"+start[4:6]+"-"+start[6:8]+"T"+start[8:]
+    end = result.pop('End')
+    end = end[0:4]+"-"+end[4:6]+"-"+end[6:8]+"T"+end[8:]
+    end = pd.to_datetime(end,yearfirst=True,utc=False).tz_localize(result['timezone']).tz_convert('UTC')
+    start=pd.to_datetime(start,yearfirst=True,utc=False).tz_localize(result['timezone']).tz_convert('UTC')
+    result['End_time']=end
+    result['Start_time']=start
+
+    result = pd.Series(result).rename(result['ID'])
+    return result
+
+def read_gsdp_data(meta_data:pd.Series) -> pd.Series:
+    """
+    Read data in given a meta data record
+    Args:
+        meta_data: meta data record as a pandas series.
+    Returns: pandas series with actual information. Missing data is set to null. Will have name = ID
+
+    """
+    freq_transform = {'1hr':'1h'} # how we transfer timestep info into pandas freq stings.
+
+    index = pd.date_range(start=meta_data.Start_time,end=meta_data.End_time,freq=freq_transform[meta_data.timestep])
+    series = pd.read_csv(meta_data.file,skiprows=meta_data.header_lines-1).set_index(index)
+    series = series.iloc[:,0]
+    series = series.rename(meta_data.name) # rename
+    series = series.astype('float')
+    # set all values which are "No data value" to null
+    series = series.replace(meta_data['No data value'],np.nan)
+
+    return series
+
+
+def max_process(ds: xarray.Dataset,
+                time_dim: str = 'time',
+                minf_mean: float = 0.8,
+                minf_max: float = 0.8) -> typing.Optional[xarray.Dataset]:
+    """
+    Process dataset for max (and mean & time of max).
+
+    Args:
+        minf_max: The minimum fraction of samples in ds to compute maximum
+        minf_mean: The minimum fraction used to compute a time-mean
+        time_dim: the name of the time dimension
+        ds: dataset to be processed. Should contain the variables:
+            rain -- mean rain
+            fraction -- fraction of data present in the mean computation.
+            longitude & latitude - longitude and latitude co-ords.
+        Note this dataset will be loaded.
+
+    Returns:dataset containing maximum values, time of max values and mean values.
+    (or None if no non-missing data).
+
+    """
+    ds.load()
+    bad = ds.rain.isnull().all(time_dim, keep_attrs=True)  # find out where *all* data null
+    if bad.all():
+        my_logger.warning(f"All missing at {bad[time_dim].isel[[0, -1]]}")
+        return None
+    max_rain = ds.rain.max(time_dim, keep_attrs=True, skipna=True)
+    max_fraction = (ds.fraction >= minf_mean).mean(time_dim)
+    max_rain = max_rain.where(max_fraction >= minf_max).rename('max_rain')
+    mean_rain = ds.rain.mean(time_dim, keep_attrs=True, skipna=True)
+    mean_rain = mean_rain.where(max_fraction >= minf_max).rename('mean_rain')
+    indx = xarray.where(bad, 0.0, ds.rain).argmax(dim=time_dim, keep_attrs=True)
+    max_time = ds[time_dim].isel({time_dim: indx}).where(~bad).rename('time_max_rain')  # max times for this season
+
+
+    base_name = max_rain.attrs['long_name']
+
+    max_fraction.attrs['long_name'] = "Fraction present " + base_name
+    max_fraction.attrs['units'] = '1'
+
+    result = xarray.Dataset(
+        dict(max_rain=max_rain, time_max_rain=max_time, mean_rain=mean_rain, max_fraction=max_fraction))
+
+    result.attrs.update(ds.attrs)
+    return result
+
+
+def process_gsdp_record(gsdp_record:pd.Series,
+                        resample_max:str='QS-DEC'
+                        ) -> pd.DataFrame:
+    """
+
+    Args:
+        gsdp_record: pandas series containing hourly rainfall
+        resample_max: time period over which to resample
+
+    Returns:Dataframe containing (as columns) indexed by time:
+      #fraction -- fraction of non-missing data
+      max -- value of max
+      mean -- mean value
+      time_max -- time max.
+
+    """
+    resamp = gsdp_record.resample(resample_max)
+    all = gsdp_record.isnull().resample(resample_max).count()
+    fraction = (resamp.count()/all).rename('fraction')
+    max = resamp.max().rename("max_rain")
+    time_max = resamp.apply(pd.Series.idxmax).rename("time_max_rain")
+    # fix the type for time_max
+    time_max = time_max.astype(gsdp_record.index.dtype)
+    mean = resamp.mean().rename("mean_rain") # need to convert to mm/season?
+    total = (mean*all).rename('total_rain')
+    df = pd.DataFrame([max,mean,total,fraction,time_max]).T
+    for s in ['max_rain','mean_rain','total_rain','fraction']:
+        df[s]=df[s].astype('float')
+    df['time_max']=df['time_max_rain'].astype(gsdp_record.index.dtype)
+
+    return df
 
 def process_radar(dataSet: xarray.Dataset,
                   mean_resample: str = '1h',
@@ -18,19 +211,22 @@ def process_radar(dataSet: xarray.Dataset,
                   max_resample: str = 'QS-DEC',
                   min_max: float = 0.8,
                   time_dim: str = 'time',
+                  mask: typing.Optional[xarray.DataArray] = None,
                   radar_range: float = 150e3,
                   ) -> xarray.Dataset:
     """
     Compute max rainfall on mean rainfall from 6 minute radar rainfall data.
 
-      :param dataSet: dataset to be processed. Must contain rainrate & isfile
+      :param dataSet: dataset to be processed. Must contain rainrate, isfile, latitude and longitude
       :param mean_resample: time period to resample data to compute mean
       :param min_mean: min fraction of samples to compute mean.
       :param max_resample : time period to resample meaned data to compute max.
       :param min_max: min fraction of samples to compute max from
       :param time_dim:dimension that is time
+      :param mask: Bool mask. Where True when isfile is 1 missing values will be set to 0.0
+         If None then will construct mask using radar_range
       :param radar_range: distance from centre to be in radar coverage --
-        missing values in this range  will be set to zero if isfile is 1.
+        missing values in this range  will be set to zero if isfile is 1 but only if mask is required.
       :return: max of meaned data with missing data dealt with...
 
 
@@ -40,7 +236,8 @@ def process_radar(dataSet: xarray.Dataset,
     # step 1 -- compute the mean of the radar data,
     my_logger.info("Starting data processing")
     resamp_mean = dataSet.resample({time_dim: mean_resample})
-    in_range = (dataSet.x.astype('float') ** 2 + dataSet.y.astype('float') ** 2) < radar_range ** 2
+    if mask is None:
+        mask = (dataSet.x.astype('float') ** 2 + dataSet.y.astype('float') ** 2) < radar_range ** 2
     # this range is a little large as we have a ring of cells which are always 0.
     rain = []  # list of all rain values
     fraction = []  # fraction of OK values
@@ -48,7 +245,7 @@ def process_radar(dataSet: xarray.Dataset,
         coord = {time_dim: c}  # coordinate to set data time
         r = ds.rainrate
         # now to set missing data where data for time is OK and in range of radar to zero.
-        r2 = xarray.where(((ds.isfile == 1) & r.isnull() & in_range), 0.0, r, keep_attrs=True)
+        r2 = xarray.where(((ds.isfile == 1) & r.isnull() & mask), 0.0, r, keep_attrs=True)
         fract = ds.isfile.mean(time_dim)
         r2 = r2.mean(time_dim, skipna=True, keep_attrs=True)  # compute mean
         r2 = xarray.where(fract >= min_mean, r2, np.nan)
@@ -70,49 +267,41 @@ def process_radar(dataSet: xarray.Dataset,
 
     ## step 2 -- compute the max using the max resampler.
     resamp_max = xarray.Dataset(dict(rain=rain, fraction=fraction)).resample({time_dim: max_resample})
-    max_rain = []
-    max_fraction = []
-    max_time = []
-    mean_rain = []
-    for c, ds in resamp_max:
-        ds.load()
-        bad = ds.rain.isnull().all(time_dim, keep_attrs=True)  # find out where *all* data null
-        if bad.all():
-            my_logger.warning(f"All missing at {c}")
-            continue  # skip further processing as all missing.
-        coord = {time_dim: c}
-        r = ds.rain.max(time_dim, keep_attrs=True, skipna=True)
-        f = (ds.fraction >= min_mean).mean(time_dim).assign_coords(coord)
-        r = r.where(f >= min_max).rename('max_rain').assign_coords(coord)
-        mn_r = ds.rain.mean(time_dim, keep_attrs=True, skipna=True)
-        mn_r = mn_r.where(f >= min_max).rename('mean_rain').assign_coords(coord)
-        indx = xarray.where(bad, 0.0, ds.rain).argmax(dim=time_dim, keep_attrs=True)
-        mtime = ds[time_dim].isel({time_dim: indx}).where(~bad).rename('time_max_rain').assign_coords(
-            coord)  # max times for this season
-        max_rain.append(r)
-        max_fraction.append(f)
-        max_time.append(mtime)
-        mean_rain.append(mn_r)
-        my_logger.info(f"Max processing {max_resample} done for {c}")
-
-    max_rain = xarray.concat(max_rain, dim=time_dim)
-    max_time = xarray.concat(max_time, dim=time_dim)
-    max_fraction = xarray.concat(max_fraction, dim=time_dim)
-    mean_rain = xarray.concat(mean_rain, dim=time_dim)
-    # fix meta data
-    base_name = max_rain.attrs['long_name']
-    max_rain.attrs['long_name'] = max_resample + " max " + base_name
-    mean_rain.attrs['long_name'] = max_resample + " mean " + base_name
-    max_time.attrs['long_name'] = max_resample + " max time " + base_name
-    max_fraction.attrs['long_name'] = "Fraction present " + base_name
-    max_fraction.attrs['units'] = '1'
+    result = resamp_max.map(max_process,
+                            time_dim=time_dim,
+                            minf_mean=min_mean,
+                            minf_max=min_max)
+    # include lat/long for each var.
+    dataSet.latitude.load()
+    dataSet.longitude.load()
+    for var in ["time_max_rain", "max_rain", "mean_rain"]:
+        result[var]=result[var].assign_coords(latitude=dataSet.latitude,
+                                              longitude=dataSet.longitude)
+    base_name = result.max_rain.attrs['long_name']
+    result.max_rain.attrs['long_name'] = max_resample + " max " + base_name
+    result.mean_rain.attrs['long_name'] = max_resample + " mean " + base_name
+    result.time_max_rain.attrs['long_name'] = max_resample + " max time " + base_name
+    # add in the fraction
     fraction2 = fraction.rename({time_dim: time_dim + "_" + mean_resample})  # rename the time dim for fract present
-    result = xarray.Dataset(
-        dict(max_rain=max_rain, max_rain_time=max_time, mean_rain=mean_rain, max_fraction=max_fraction,
-             fraction=fraction2))
+    result['fraction']=fraction2
     result.attrs.update(dataSet.attrs)
-    my_logger.info(f"Completed processing of {len(result.fraction)} hours")
     return result
+
+
+
+def memory_use() -> str:
+    """
+
+    Returns: a string with the memory use in Gbytes or "Mem use unknown" if unable to load resource
+     module.
+
+    """
+    try:
+        import resource
+        mem = f"Mem = {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6} Gbytes"
+    except ModuleNotFoundError:
+        mem = 'Mem use unknown'
+    return mem
 
 
 def resample_max(data_array: xarray.DataArray,
@@ -124,7 +313,7 @@ def resample_max(data_array: xarray.DataArray,
     :param data_array: data array to be processed.
     :param resample: resample value 
     :param time_dim: name of time dimension
-    :param fixNan -- if True set Nan to zero.
+    :param fillna -- if not Null value to fill nan values with.
     :return: DataSet of processed dataArrays. Computation is resample mean then seasonal max.
      Variables are:
          Max_{dataArray.name}  (max value in a season)
@@ -161,8 +350,7 @@ def resample_max(data_array: xarray.DataArray,
             continue  # skip further processing as all missing.
         # need to load the data as dask does not cope with complex indixing
         da.load()
-        mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6
-        my_logger.debug(f"Loaded data for coord {c}. Memory = {mem} GBytes")
+        my_logger.debug(f"Loaded data for coord {c}. {memory_use()}")
         indx = da.argmax(dim=time_dim, skipna=True, keep_attrs=True)  # index of maxes
         coord = {time_dim: c}  # coordinate to set data time
         mtime = da[time_dim].isel({time_dim: indx}).where(~bad).rename('time_max').assign_coords(
@@ -172,11 +360,10 @@ def resample_max(data_array: xarray.DataArray,
         # then store things.
         mx_time.append(mtime)
         mx_value.append(mvalue)
-        mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6  # in Gbytes
-        my_logger.debug(f"Computed mx value and time of max at time {c}. Mem = {mem} Gbytes")
+        my_logger.debug(f"Computed mx value and time of max at time {c}. {memory_use()}")
     mx_time = xarray.concat(mx_time, dim=time_dim).sortby(time_dim)  # concat everything
     mx_value = xarray.concat(mx_value, dim=time_dim).sortby(time_dim)
-    my_logger.debug(f"Concatted. Mem = {mem}")
+    my_logger.debug(f"Concatted. {memory_use()}")
     # set up attributes.
     mx_value.attrs = data_array.attrs.copy()
     mx_value.attrs['history'] = mx_value.attrs.get('history', []) + [f"Resampled {resample} + seasonal max"]
--- test_ausLib.py
+++ test_ausLib.py
@@ -1,20 +1,60 @@
+import pathlib
 import unittest
+
+import numpy as np
+import pandas as pd
 import xarray
 import ausLib
 
+test_dir = pathlib.Path("test_data")
 class test_ausLib(unittest.TestCase):
-    def test_something(self):
-        dataSet = xarray.open_mfdataset("/g/data/rq0/level_2/28/RAINRATE/28_2000*_rainrate.nc")
-        mean_resample = '1h'
-        max_resample = '1MS'
-        min_mean = 0.8
-        min_max = 0.8
-        time_dim = 'time'
-        radar_range = 150e3
+
+    def test_process_radar(self):
+        dataSet = xarray.open_mfdataset(test_dir/"28_19980924_rainrate.nc")
         # test is to just run the code!
         dataSet = ausLib.process_radar(dataSet)
+        # test data is 1 day. So expect to have only one field in the output.
+        self.assertEqual(dataSet.time.size,1)
+        # expect max >= mean.
+        null = dataSet.max_rain.isnull() & dataSet.mean_rain.isnull() #
+        self.assertTrue(np.all((dataSet.max_rain >= dataSet.mean_rain).where(~null,True)))
+        # test time_max_rain mask matches null.
+        self.assertTrue(np.all(dataSet.time_max_rain.isnull() == null))
 
-        self.assertEqual(True, False)  # add assertion here
+    def test_read_gdsp_metadata(self):
+        # test read_gdsp_metadata
+        series = ausLib.read_gsdp_metadata(test_dir/"AU_088162.txt")
+        self.assertIsInstance(series,pd.Series)
+        # assert the name is the ID.
+        self.assertEqual(series.name,series.ID)
+        # assert have 21 lines in the header.
+        self.assertEqual(series.header_lines,21)
+
+    def test_read_gsdp_data(self):
+        # test that read_data works
+        meta_data = ausLib.read_gsdp_metadata(test_dir/"AU_088162.txt")
+        series = ausLib.read_gsdp_data(meta_data)
+        # len of series should be as expected from meta-data
+        nrec= meta_data.loc['Number of records']
+        self.assertEqual(len(series),nrec)
+        # and fraction missing should be as expected.
+        percent_missing = float(100*series.isnull().sum())/nrec
+        self.assertAlmostEquals(percent_missing,meta_data.loc['Percent missing data'],places=2)
+
+    def test_gsdp_metadata(self):
+        # test gsdp_metadata
+        files=test_dir.glob("AU_*.txt")
+        meta_data = ausLib.gsdp_metadata(files)
+        self.assertIsInstance(meta_data,pd.DataFrame)
+        # can't think of other checks...
+
+    def test_process_gsdp_record(self):
+        # test process_gsdp_record
+        meta_data = ausLib.read_gsdp_metadata(test_dir / "AU_088162.txt")
+        series = ausLib.read_gsdp_data(meta_data)
+        process = ausLib.process_gsdp_record(series)
+        # test max >= mean
+        self.assertTrue((process.max_rain >= process.mean_rain).all())
 
 
 if __name__ == '__main__':
